7a57807ee59d4ffac3e2914b54bb69283374e8fa
Fabio M. Di Nitto <fdinitto@redhat.com>
2023-06-07 09:08:42 +0200

    pipelines: rework fs structure and include
    add support for multiple branches in knet-ci-test templates
    
    Signed-off-by: Fabio M. Di Nitto <fdinitto@redhat.com>

--- pipelines/vars/getAuthCheck.groovy
+++ pipelines/vars/getAuthCheck.groovy
@@ -1,34 +1,41 @@
 // Check if a PR has been submitted by an authorized user
-def call() {
+def call(Map params) {
+    // requirered param
+    isPullRequest = params["isPullRequest"]
 
     sh 'env|sort'
+
+    println("isPullRequest: ${isPullRequest}")
+
     bc = currentBuild.getBuildCauses()
     println("Build Cause: ${bc}")
 
     // Triggered by admin on the website
     userEvent = currentBuild.getBuildCauses('hudson.model.Cause$UserIdCause')
     if (!userEvent.size().equals(0)) {
+	println("Build Triggered from Jensink web UI")
 	return true
     }
 
-    // Triggered by admin comment in the PR
-    userEvent = currentBuild.getBuildCauses('com.adobe.jenkins.github_pr_comment_build.GitHubPullRequestCommentCause') 
-    if (!userEvent.size().equals(0)) {
-	return true
-    }
-
-    // Commit from a branch
-    branchEvent = currentBuild.getBuildCauses('jenkins.branch.BranchEventCause')
-    if (!branchEvent.size().equals(0) && env.BRANCH_NAME == "main") {
+    if (isPullRequest == false) {
+	println("Build Triggered by merge into branch")
 	// This is a merge into main, so allowed.
 	// Note that this still works if the user's branch is called "main" because
 	// Jenkins creates its own branch named after the PR
 	return true;
     }
-    
+
+    // Triggered by admin comment in the PR
+    userEvent = currentBuild.getBuildCauses('com.adobe.jenkins.github_pr_comment_build.GitHubPullRequestCommentCause') 
+    if (!userEvent.size().equals(0)) {
+	println("Build Triggered from PR comment")
+	return true
+    }
+
     // Caused by a PR. Check it's from a valid user
     valid_admins = getValidPRUsers()
     if (valid_admins.contains(env.CHANGE_AUTHOR)) {
+	println("Build Triggered by PR authorized user")
 	return true
     }
 
@@ -46,6 +53,8 @@ def call() {
     result = input(message: "Please verify this is safe to run", ok: "OK",
 		   submitterParameter: 'submitter')
     println(result)
+
+    println("Build Triggered by PR manual approval")
 
     // If we get this far then it's been approved. Abort is the other option!
     return true

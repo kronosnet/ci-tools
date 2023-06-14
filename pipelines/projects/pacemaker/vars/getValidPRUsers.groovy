8ef1f236161e1bb7df3120244aad5162d6eac8f5
Fabio M. Di Nitto <fdinitto@redhat.com>
2023-06-14 17:31:58 +0200

    Move pacemaker to pipelines
    Signed-off-by: Fabio M. Di Nitto <fdinitto@redhat.com>

---
+++ pipelines/projects/pacemaker/vars/getValidPRUsers.groovy
@@ -0,0 +1,4 @@
+// Imported from githubpr config + whitelist
+def call() {
+    return ['aleksei-burlakov', 'angelabriel', 'arbulu89', 'ayoub-belarbi', 'chrissie-c', 'clumens', 'CtrlZmaster', 'diegoakechi', 'dmuhamedagic', 'epenchev', 'fdanapfel', 'feist', 'fghaas', 'HideoYamauchi', 'houndci-bot', 'idevat', 'ioguix', 'jfriesse', 'jjzhang', 'kskmori', 'lhh', 'liangxin1300', 'mirecheck', 'nrwahl2', 'oalbrigt', 'ondrejmular', 'phmarek', 'rtorrero', 'stefanotorresi', 'tserong', 'varkoly', 'waltdisgrace', 'zzhou1']
+}

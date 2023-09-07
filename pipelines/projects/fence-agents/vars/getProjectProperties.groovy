acdfa040ed63b58d9ce3850e3a15aa9bd6a4edb9
Fabio M. Di Nitto <fdinitto@redhat.com>
2023-09-07 05:48:10 +0200

    Move fence-agents to new pipelines
    Signed-off-by: Fabio M. Di Nitto <fdinitto@redhat.com>

---
+++ pipelines/projects/fence-agents/vars/getProjectProperties.groovy
@@ -0,0 +1,27 @@
+// Return options for this build.
+// Return a map of key=value pairs that will
+// be passed into the environment on the building node.
+//
+// info contains all of the global build information
+//      (do NOT change anything in here)
+// extras contains 'extra' variables added for this run only
+//      during the buildRunMap phase
+// agentName is the node we are building on,
+// branch is the git branch we are building (for)
+def call(Map info, Map extras, String agentName, String branch)
+{
+    def props = [:]
+
+    props['MAKEOPTS'] = ''
+    props['PARALLELMAKE'] = ''
+    props['MAKEINSTALLOPTS'] = ''
+    props['TOPTS'] = ''
+    props['EXTRACHECKS'] = ''
+    props['EXTERNAL_LD_LIBRARY_PATH'] = ''
+    props['SPECVERSION'] = env.BUILD_NUMBER
+    props['RPMDEPS'] = 'corosynclib-devel'
+
+    props['DISTROCONFOPTS'] = '--enable-cpg-plugin'
+
+    return props
+}

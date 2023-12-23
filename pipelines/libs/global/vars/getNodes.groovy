d45daa1ef2b4db44603981a9af24eb6814a9ee43
Fabio M. Di Nitto <fdinitto@redhat.com>
2023-12-23 08:21:42 +0100

    Update anvil bm phy01
    Signed-off-by: Fabio M. Di Nitto <fdinitto@redhat.com>

--- pipelines/libs/global/vars/getNodes.groovy
+++ pipelines/libs/global/vars/getNodes.groovy
@@ -3,7 +3,7 @@
 def call()
 {
     def labels = [:]
-    labels['anvil-ci-bm-phy01'] = ['centos-8','stable','x86-64','rpm','yum','anvil-bm']
+    labels['anvil-ci-bm-phy01'] = ['almalinux-9','stable','x86-64','rpm','yum','anvil-bm']
     labels['anvil-ci-bm-phy02'] = ['centos-8','stable','x86-64','rpm','yum','anvil-bm']
     labels['anvil-ci-bm-phy03'] = ['centos-8','stable','x86-64','rpm','yum','anvil-bm']
     labels['anvil-ci-bm-phy04'] = ['centos-8','stable','x86-64','rpm','yum','anvil-bm']

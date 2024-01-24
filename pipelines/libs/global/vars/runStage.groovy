b1405b0c51d22d59269a449a2cdbf2de2ed2e301
Fabio M. Di Nitto <fdinitto@redhat.com>
2024-01-24 12:22:05 +0100

    Revert "Refactor runStage so that all of the stages are run"
    This reverts commit d1429a823b0efd8e72973d810830d188fb090268.

--- pipelines/libs/global/vars/runStage.groovy
+++ pipelines/libs/global/vars/runStage.groovy
@@ -1,5 +1,5 @@
 // Run a generic CI stage with the right params and record success/failure
-// This is here most of the main gubbins happens
+
 def call(Map info, String agentName, String stageName, Boolean voting, Map extravars)
 {
     println('doRunStage info='+info+', extravars='+extravars)
@@ -31,6 +31,7 @@ def call(Map info, String agentName, String stageName, Boolean voting, Map extra
     }
 }
 
+
 def doRunStage(String agentName, Map info, Map localinfo)
 {
     // Timeout (minutes) for the collection stages
@@ -46,110 +47,64 @@ def doRunStage(String agentName, Map info, Map localinfo)
 	stageTitle = localinfo['title']
     }
 
-    // Things to run - in order
-    // groovy uses a LinkedHashMap so they retain insertion order
-    def stages = [:]
-    stages['ci-build-info'] = 'Build info'
-    stages['ci-setup-rpm'] = 'Setup RPMs'
-    stages['ci-setup-src'] = 'Setup source'
-    stages['ci-build-src'] = 'Build source'
-    stages['ci-tests-src'] = 'Run tests'
-    stages['ci-install-src'] = 'Install'
-
-    // Run stuff! On this node!
+    // Run stuff!
     node("${agentName}") {
 	echo "Building ${stageTitle} for ${localinfo['project']} on ${agentName} (${localinfo['stageType']})"
-
-	stage("Cleaning workspace") {
-	    cleanWs(disableDeferredWipeout: true, deleteDirs: true)
-	}
+	cleanWs(disableDeferredWipeout: true, deleteDirs: true)
 
 	// Save the local workspace directory for later
 	def workspace = env.WORKSPACE + '/' + localinfo['project']
 
-	// Update run stats
 	info["${localinfo['stageType']}_run"]++
-
-	// This is the log file we will add to the artifacts
-	stagestate['logfile'] = "${localinfo['stageName']}-${agentName}.log"
-
-	// Keep the logs in separate files per node/function so they are easy to find
-	tee (stagestate['logfile']) {
-	    stage("Checkout") {
-		stagestate['runstage'] = 'checkout'
-		def rc = runWithTimeout(collect_timeout, { getSCM(info) }, stagestate,
-					{ processRunSuccess(info, localinfo, stagestate) },
-					{ processRunException(info, localinfo, stagestate) })
-		if (rc != 'OK') {
-		    println("RC runWithTimeout returned "+rc)
-		    // Big stick here, we can't continue
-		    shNoTrace("exit 1", "Marking this stage as a failure")
-		}
-	    }
-
-	    stage("Collect node info") {
-
-		// Add node-specific properties
-		localinfo += getNodeProperties(agentName)
-
-		// Get any job-specific configuration variables
-		localinfo += getProjectProperties(localinfo, agentName)
-
-		// Converting ci-tools/ci-set-env to groovy maps
-		localinfo += ci_set_env(localinfo, localinfo['stageName'], agentName)
-	    }
-
-	    def exports = getShellVariables(localinfo)
-	    def build_timeout = getBuildTimeout()
-	    def running = true
-
-	    // Run all the shell stages
-	    for (stageinfo in stages) {
-		if (running) { // break does weird shit
-		    stage("${stageinfo.value}") {
-			stagestate['runstage'] = stageinfo.value
-
-			// Run everything in the checked-out directory
-			dir (localinfo['project']) {
-			    cmdWithTimeout(build_timeout,
-					   "${exports} $HOME/ci-tools/ci-wrap ${stageinfo.key}",
-					   stagestate,
-					   { processRunSuccess(info, localinfo, stagestate) },
-					   { processRunException(info, localinfo, stagestate) })
-			}
-
-			// This marks it red in the graph view if it failed
-			if (stagestate['failed']) {
-			    catchError(buildResult: 'SUCCESS', stageResult: 'FAILURE') {
-				shNoTrace("exit 1", "Marking this stage as a failure")
-			    }
-			    // Don't run any more shell stages if one fails
-			    running = false
-			}
-		    }
-		}
+	stage("${stageTitle} on ${agentName} - checkout") {
+	    stagestate['runstage'] = 'checkout'
+	    def rc = runWithTimeout(collect_timeout, { getSCM(info) }, stagestate,
+				    { processRunSuccess(info, localinfo, stagestate) },
+				    { processRunException(info, localinfo, stagestate) })
+	    if (rc != 'OK') {
+		println("RC runWithTimeout returned "+rc)
+		// Big stick here, we can't continue
+		sh "exit 1"
 	    }
 	}
 
-	// Save the log (if it exists)
-	if (stagestate.containsKey('logfile')) {
-	    info['have_split_logs'] = true
-	    dir (env.WORKSPACE) {
-		if (stagestate['failed']) {
-		    // Rename the log so we know it all went badly
-		    sh "mv ${stagestate['logfile']} FAILED_${stagestate['logfile']}"
-		    archiveArtifacts artifacts: "FAILED_${stagestate['logfile']}", fingerprint: false
-		} else {
-		    // Rename the log so we know it all went fine
-		    sh "mv ${stagestate['logfile']} SUCCESS_${stagestate['logfile']}"
-		    archiveArtifacts artifacts: "SUCCESS_${stagestate['logfile']}", fingerprint: false
+	// Add node-specific properties
+	localinfo += getNodeProperties(agentName)
+
+	// Get any job-specific configuration variables
+	localinfo += getProjectProperties(localinfo, agentName)
+
+	// Converting ci-tools/ci-set-env to groovy maps
+	localinfo += ci_set_env(localinfo, localinfo['stageName'], agentName)
+
+	def build_timeout = getBuildTimeout()
+
+	stage("${stageTitle} on ${agentName} - build") {
+	    stagestate['logfile'] = "${localinfo['stageName']}-${agentName}.log"
+	    stagestate['runstage'] = 'run'
+
+	    // Keep the log in a separate file so they are easy to find
+	    tee (stagestate['logfile']) {
+		// Run everything in the checked-out directory
+		dir (localinfo['project']) {
+		    def exports = getShellVariables(localinfo)
+		    cmdWithTimeout(build_timeout,
+				   "${exports} $HOME/ci-tools/ci-wrap ci-build",
+				   stagestate,
+				   { processRunSuccess(info, localinfo, stagestate) },
+				   { processRunException(info, localinfo, stagestate) })
+		}
+	    }
+	    // This marks it red in the graph view
+	    if (stagestate['failed']) {
+		catchError(buildResult: 'SUCCESS', stageResult: 'FAILURE') {
+		    shNoTrace("exit 1", "Marking this stage as a failure")
 		}
 	    }
 	}
 
 	println('STAGESTATE: '+stagestate)
-
-	// Don't run gather jobs if this bit failed
+	// Run dependant global scripts for some jobs ... if we suceeded
 	if (stagestate['failed']) {
 	    println('this job failed, collection not happening')
 	    return false
@@ -159,11 +114,11 @@ def doRunStage(String agentName, Map info, Map localinfo)
 	// to de-duplicate these at the end, in postStage()
 	info['EXTRAVER_LIST'] += localinfo['extraver']
 
-	// Gather any covscan results
+	// Gather covscan results
 	// 'fullrebuild' is param set by a parent job to prevent uploads from weekly jobs
 	//    and will normally be 0
 	if (localinfo['stageName'].endsWith('covscan') && localinfo['fullrebuild'] == 0) {
-	    stage("Get covscan artifacts") {
+	    stage("${localinfo['stageName']} on ${agentName} - get covscan artifacts") {
 		// Yes - you can nest 'node's
 		node('built-in') {
 		    if (localinfo['isPullRequest']) {
@@ -185,9 +140,9 @@ def doRunStage(String agentName, Map info, Map localinfo)
 	    }
 	}
 
-	// Gather any RPM builds
+	// RPM builds
 	if (localinfo['stageName'].endsWith('buildrpms') && localinfo['publishrpm'] == 1 && localinfo['fullrebuild'] == 0) {
-	    stage("Get RPM artifacts") {
+	    stage("${localinfo['stageName']} on ${agentName} - get RPM artifacts") {
 		node('built-in') {
 		    if (localinfo['isPullRequest']) {
 			// TODO: fix pr path and adjust for 'extraver'
@@ -206,17 +161,23 @@ def doRunStage(String agentName, Map info, Map localinfo)
 	    }
 	}
 
-	// Tidy the workspace (remember we are still on the build node here)
-	stage("Cleanup") {
-	    cleanWs(disableDeferredWipeout: true, deleteDirs: true)
-	}
+	cleanWs(disableDeferredWipeout: true, deleteDirs: true)
     }
     return true
 }
 
-// It went well
 def processRunSuccess(Map info, Map localinfo, Map stagestate)
 {
+    // Rename the log so we know it all went fine
+    // The log file might not be there, if getSCM failed.
+    if (stagestate.containsKey('logfile')) {
+	dir('..') {
+	    sh "mv ${stagestate['logfile']} SUCCESS_${stagestate['logfile']}"
+	    archiveArtifacts artifacts: "SUCCESS_${stagestate['logfile']}", fingerprint: false
+	    info['have_split_logs'] = true
+	}
+    }
+
     stagestate['failed'] = false
 }
 
@@ -230,12 +191,23 @@ def processRunException(Map info, Map localinfo, Map stagestate)
     // as environment variable names.
     def stage = localinfo['stageName'].replace('-','_')
 
-    def runtype = stagestate['runstage']
-
+    def runtype = ''
+    if (stagestate['runstage'] == 'checkout') {
+	runtype = ' source download'
+    }
     // Stats, and save the job name for the email
     info["${localinfo['stageType']}_fail"]++
-    info["${localinfo['stageType']}_fail_nodes"] += env.NODE_NAME + "(${localinfo['stageName']} ${runtype})" + ' '
+    info["${localinfo['stageType']}_fail_nodes"] += env.NODE_NAME + "(${localinfo['stageName']}${runtype})" + ' '
     info["${stage}_failed"] = 1 // One of these failed. that's all we need to know
+
+    // The log file might not be there, if getSCM failed.
+    if (stagestate.containsKey('logfile')) {
+	dir ('..') {
+	    sh "mv ${stagestate['logfile']} FAILED_${stagestate['logfile']}"
+	    archiveArtifacts artifacts: "FAILED_${stagestate['logfile']}", fingerprint: false
+	    info['have_split_logs'] = true
+	}
+    }
 
     // If the jobs was aborted, then GO AWAY!
     if (stagestate['RET'] == 'ABORT') {
@@ -247,8 +219,7 @@ def processRunException(Map info, Map localinfo, Map stagestate)
     if (localinfo['stageName'].endsWith('covscan') && fileExists('cov.json')) {
 	stagestate['failed'] = false // Coverity failed but we want the results
 
-	// Are there any new covscan errors?
-	if (fileExists('cov.html/new/index.html')) {
+	if (fileExists('cov.html/new/index.html')) { // New errors
 	    stagestate['new_cov_errors'] = true
 	}
     } else {

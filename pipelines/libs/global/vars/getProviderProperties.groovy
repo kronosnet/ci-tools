74d3c4a2da667171aace4ca474f19d747efc42da
Fabio M. Di Nitto <fdinitto@redhat.com>
2024-04-22 06:45:08 +0200

    fn-testing: fix up providers
    - disable ocvp for now
    - enable libvirtd in weekly
    - disable libvirtd rhel7 testing
    
    Signed-off-by: Fabio M. Di Nitto <fdinitto@redhat.com>

--- pipelines/libs/global/vars/getProviderProperties.groovy
+++ pipelines/libs/global/vars/getProviderProperties.groovy
@@ -4,11 +4,11 @@ def call()
     // Cloud providers and their limits
     def providers = [:]
 
+    providers['libvirtd'] = ['maxjobs': 1, 'testlevel': 'all', 'rhelvers': ['8', '9'], 'has_watchdog': true, 'has_storage': true, 'weekly': true]
     providers['osp'] = ['maxjobs': 3, 'testlevel': 'all', 'rhelvers': ['8', '9'], 'has_watchdog': true, 'has_storage': true, 'weekly': true]
-    providers['ocpv'] = ['maxjobs': 3, 'testlevel': 'all', 'rhelvers': ['8', '9'], 'has_watchdog': true, 'has_storage': false, 'weekly': true]
-    providers['libvirtd'] = ['maxjobs': 1, 'testlevel': 'all', 'rhelvers': ['7', '8', '9'], 'has_watchdog': true, 'has_storage': true, 'weekly': false]
-    //    providers['ibmvpc'] = ['maxjobs': 0, 'testlevel': 'all', 'rhelvers': ['8','9'], 'has_watchdog': false]
-    //    providers['aws'] = ['maxjobs': 1, 'testlevel': 'smoke', 'rhelvers': ['8', '9'], 'has_watchdog': true]
+    //providers['ocpv'] = ['maxjobs': 3, 'testlevel': 'all', 'rhelvers': ['8', '9'], 'has_watchdog': true, 'has_storage': false, 'weekly': true]
+    //providers['ibmvpc'] = ['maxjobs': 0, 'testlevel': 'all', 'rhelvers': ['8','9'], 'has_watchdog': false]
+    //providers['aws'] = ['maxjobs': 1, 'testlevel': 'smoke', 'rhelvers': ['8', '9'], 'has_watchdog': true]
 
     return providers
 }

308d02fa19e10a2378cd4ae27a242d677cc085fe
Fabio M. Di Nitto <fdinitto@redhat.com>
2024-06-19 06:32:26 +0200

    bootstrap
    Signed-off-by: Fabio M. Di Nitto <fdinitto@redhat.com>

---
+++ pipelines/projects/knet-ci-test-gitlab/vars/getEmailReplyTo.groovy
@@ -0,0 +1,7 @@
+// Return a string containing any email addresses that should
+// appear in a Reply-To: header on the completion emails.
+// If '' is returned then a default will be supplied
+def call()
+{
+    return 'ccaulfie@redhat.com'
+}

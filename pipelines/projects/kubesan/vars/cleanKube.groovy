4c0895644be0f3e89dac6119d44f6e62c42c761a
Fabio M. Di Nitto <fdinitto@redhat.com>
2024-06-26 08:18:24 +0200

    kubesan: cleanup temporary files
    Signed-off-by: Fabio M. Di Nitto <fdinitto@redhat.com>

--- pipelines/projects/kubesan/vars/cleanKube.groovy
+++ pipelines/projects/kubesan/vars/cleanKube.groovy
@@ -3,5 +3,7 @@ def call() {
 	script: 'su - kubesan /bin/bash -c "minikube delete --all"')
     sh (label: 'Remove all old podman images',
 	script: 'su - kubesan /bin/bash -c "podman image rm --force --all"')
+    sh (label: 'Remove minikube temp files',
+	script: 'rm -rf /tmp/minikube* /tmp/juju*')
     return ''
 }

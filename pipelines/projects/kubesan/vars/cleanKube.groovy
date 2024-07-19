04a0afee79f89e01e24cfa0d5932d3c776fa32c7
Fabio M. Di Nitto <fdinitto@redhat.com>
2024-07-19 12:49:25 +0200

    kubesan: make sure to remove all kcli clusters
    Signed-off-by: Fabio M. Di Nitto <fdinitto@redhat.com>

--- pipelines/projects/kubesan/vars/cleanKube.groovy
+++ pipelines/projects/kubesan/vars/cleanKube.groovy
@@ -1,4 +1,6 @@
 def call() {
+    sh (label: 'Remove all kcli clusters',
+	script: 'su - kubesan /bin/bash -c "kcli delete plan --all --yes"')
     sh (label: 'Remove all minikube clusters',
 	script: 'su - kubesan /bin/bash -c "minikube delete --all"')
     sh (label: 'Remove all old podman images',

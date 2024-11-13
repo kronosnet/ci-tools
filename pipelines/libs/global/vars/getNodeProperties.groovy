dc2c8baf78424d6d3ad5d4849632910101e74704
Fabio M. Di Nitto <fdinitto@redhat.com>
2024-11-13 10:00:33 +0100

    add ubuntu 24.10
    Signed-off-by: Fabio M. Di Nitto <fdinitto@redhat.com>

--- pipelines/libs/global/vars/getNodeProperties.groovy
+++ pipelines/libs/global/vars/getNodeProperties.groovy
@@ -63,6 +63,7 @@ def call(String node)
     node_env['ubuntu-20-04-lts-x86-64'] = []
     node_env['ubuntu-22-04-lts-x86-64'] = []
     node_env['ubuntu-24-04-lts-x86-64'] = []
+    node_env['ubuntu-24-10-x86-64'] = []
     node_env['ubuntu-devel-x86-64'] = []
 
     if (node_env.containsKey(node)) {

7e68fa12119e322a7e0a407eb13175a127a2e951
Fabio M. Di Nitto <fdinitto@redhat.com>
2025-02-23 13:13:08 +0100

    kubesan: remove minikube support
    Signed-off-by: Fabio M. Di Nitto <fdinitto@redhat.com>

--- pipelines/projects/kubesan/vars/cleanKube.groovy
+++ pipelines/projects/kubesan/vars/cleanKube.groovy
@@ -1,11 +1,7 @@
 def call() {
     sh (label: 'Remove all kcli clusters',
 	script: 'su - kubesan /bin/bash -c "kcli delete plan --all --yes"')
-    sh (label: 'Remove all minikube clusters',
-	script: 'su - kubesan /bin/bash -c "minikube delete --all"')
     sh (label: 'Remove all old podman images',
 	script: 'su - kubesan /bin/bash -c "podman image rm --force --all"')
-    sh (label: 'Remove minikube temp files',
-	script: 'rm -rf /tmp/minikube* /tmp/juju*')
     return ''
 }

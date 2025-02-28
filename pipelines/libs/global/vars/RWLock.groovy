b6542fda87645b1cadd5dc8756f7396e94828b23
Christine Caulfield <ccaulfie@redhat.com>
2025-02-28 12:42:38 +0000

    RWLock: fix things so that a) the closure runs on the current node rather than 'built-in' b) flocks get cleaned up even if the job is aborted    - requires RWLock(info, '','UNLOCK', {}) in post{always{}}

--- pipelines/libs/global/vars/RWLock.groovy
+++ pipelines/libs/global/vars/RWLock.groovy
@@ -14,32 +14,79 @@ class jnaflock {
     }
 }
 
-
-def call(String lockname, String mode, Closure thingtorun)
+// Abstracted out so we can use this for a 'normal' unlock and the 'emergency' one in post{always{}}
+def do_unlock(Map info)
 {
-    // This MUST run on the Jenkins host
     node('built-in') {
-	def lockmode = 0;
-	if (mode == 'READ') {
-	    lockmode = 1; // LOCK_SH
+	if (info.containsKey('lockfd') && info['lockfd'] >= 0) {
+	    if (jnaflock.CLibrary.INSTANCE.flock(info['lockfd'], 8) == -1) { // 8 = LOCK_UNLOCK
+		println("RWLock: unlock failed on fd ${info['lockfd']}")
+		// Don't return here, still try to close the file
+	    }
+	    if (jnaflock.CLibrary.INSTANCE.close(info['lockfd']) == 0) {
+		println("RWLock on ${info['lockname']} fd ${info['lockfd']} released")
+	    } else {
+		println("RWLock: close failed on fd ${info['lockfd']}")
+		return -1
+	    }
 	}
-	if (mode == 'WRITE') {
-	    lockmode = 2; // LOCK_EX
-	}
-	if (lockmode == 0) {
-	    throw(new Exception("jnaflock: Unknown lock mode ${mode}"))
-	    return -1
-	}
-
-	def fd = jnaflock.CLibrary.INSTANCE.creat("/var/lib/jenkins/${lockname}.lock", 0666)
-	if (fd == -1) {
-	    throw(new Exception("Failed to 'creat' file for lock ${lockname}"))
-	    return -1
-	}
-	println("FD for lock ${lockname} is " + fd)
-	jnaflock.CLibrary.INSTANCE.flock(fd, lockmode)
-	thingtorun()
-	jnaflock.CLibrary.INSTANCE.close(fd)
-	println("Lock ${lockname} released")
+	info['lockfd'] = -1
     }
+    return 0
+}
+
+def call(Map info, String lockname, String mode, Closure thingtorun)
+{
+    def lockdir = "${JENKINS_HOME}/locks"
+    def lockmode = 0
+
+    if (mode == 'READ') {
+	lockmode = 1 // LOCK_SH
+    }
+    if (mode == 'WRITE') {
+	lockmode = 2 // LOCK_EX
+    }
+    // Normally this doesn't need to be called as the closure in 'thingtorun' is
+    // run then the lock is cleared. This is for post{always{}} to tidy up
+    // in case of 'accidents'
+    if (mode == 'UNLOCK') {
+	return do_unlock(info)
+    }
+    if (lockmode == 0) {
+	throw(new Exception("RWLock: Unknown lock mode ${mode}"))
+	return -1
+    }
+
+    // Of course, this needs to be after the UNLOCK check
+    if (info.containsKey('lockfd') && info['lockfd'] >= 0) {
+	throw(new Exception("RWLock: Request for lock ${lockname}, while lock on fd ${info['lockfd']} already held (only 1 lock allowed at a time)"))
+	return -1
+    }
+
+    // This MUST run on the Jenkins host, that's where the flocks are
+    node('built-in') {
+	sh "mkdir -p ${lockdir}"
+
+	def lockfd = info['lockfd'] = jnaflock.CLibrary.INSTANCE.creat("${lockdir}/${lockname}.lock", 0666)
+	if (lockfd == -1) {
+	    throw(new Exception("RWLock: Failed to 'creat' file for lock ${lockdir}/${lockname}"))
+	    return -1
+	}
+	println("RWLock: FD for lock ${lockname} is ${lockfd}")
+	if (jnaflock.CLibrary.INSTANCE.flock(lockfd, lockmode) == -1) {
+	    throw(new Exception("RWLock: Failed to 'flock' file for lock ${lockdir}/${lockname} at ${lockmode}"))
+	    return -1
+	}
+	info['lockfd'] = lockfd
+	info['lockname'] = lockname // Save it in case of errors later
+	println("RWLock: ${lockname} locked for ${mode}")
+    }
+
+    // Run a thing inside the lock
+    thingtorun()
+
+    // Tidy up
+    do_unlock(info)
+
+    return 0
 }

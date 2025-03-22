258f6e5322f1137ef9be6060956ffe6e23389808
Fabio M. Di Nitto <fdinitto@redhat.com>
2025-03-22 16:00:29 +0100

    kubesan: use new delete method
    Signed-off-by: Fabio M. Di Nitto <fdinitto@redhat.com>

--- pipelines/projects/kubesan/vars/cleanKube.groovy
+++ pipelines/projects/kubesan/vars/cleanKube.groovy
@@ -1,6 +1,6 @@
 def call() {
     sh (label: 'Remove all kcli clusters',
-	script: 'su - kubesan /bin/bash -c "kcli delete plan --all --yes"')
+	script: 'su - kubesan /bin/bash -c "kcli start plan kubesan-test && sleep 2 && kcli delete plan --all --yes"')
     sh (label: 'Remove all old podman images',
 	script: 'su - kubesan /bin/bash -c "podman image rm --force --all"')
     return ''

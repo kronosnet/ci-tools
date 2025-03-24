f003ac071e84531afda13c0bd4c8ede53eee92fa
Christine Caulfield <ccaulfie@redhat.com>
2025-03-24 08:47:47 +0000

    prep_test_cluster: set info[:] before calling delete_cluster
    if delete_cluster fails with info[:] not set the email has missing info

--- pipelines/libs/vapor/vars/prep_test_cluster.groovy
+++ pipelines/libs/vapor/vars/prep_test_cluster.groovy
@@ -2,8 +2,8 @@ def call(Map info)
 {
     stage('Prep build env') {
 	cleanWs(disableDeferredWipeout: true, deleteDirs: true)
-	delete_cluster(info)
 	info['branch'] = "${info['provider']} ${info['osver']} zstream: ${info['zstream']} upstream: ${info['upstream']} tests: ${info['tests']}"
+	delete_cluster(info)
     }
     stage("Create ${info['provider']} ${info['osver']} test cluster") {
 	create_cluster(info)

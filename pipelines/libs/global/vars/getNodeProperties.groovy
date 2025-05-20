77f50703300e14f29bb915ad225df57abd8de7db
Fabio M. Di Nitto <fdinitto@redhat.com>
2025-05-20 06:57:19 +0200

    prep for rhel-10
    Signed-off-by: Fabio M. Di Nitto <fdinitto@redhat.com>

--- pipelines/libs/global/vars/getNodeProperties.groovy
+++ pipelines/libs/global/vars/getNodeProperties.groovy
@@ -61,6 +61,7 @@ def call(String node)
     node_env['rhel-9-vapor-rdu-4-x86-64'] = []
     node_env['rhel-9-vapor-rdu-5-x86-64'] = []
     node_env['rhel-9-vapor-rdu-6-x86-64'] = []
+    node_env['rhel-10-x86-64'] = []
     node_env['ubuntu-20-04-lts-x86-64'] = []
     node_env['ubuntu-22-04-lts-x86-64'] = []
     node_env['ubuntu-24-04-lts-x86-64'] = []

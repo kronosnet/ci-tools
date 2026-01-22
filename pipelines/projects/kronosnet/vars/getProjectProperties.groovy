f1a6e59365d520a7c8a9740d6218d8d855431ce3
Christine Caulfield <ccaulfie@redhat.com>
2026-01-22 08:18:22 +0000

    Only build knet wireshark dissector if wireshark version >= 4.6  is available

--- pipelines/projects/kronosnet/vars/getProjectProperties.groovy
+++ pipelines/projects/kronosnet/vars/getProjectProperties.groovy
@@ -10,17 +10,32 @@ def call(Map localinfo, String agentName)
 
     props['PARALLELTEST'] = 'no'
     props['RPMDEPS'] = 'libqb-devel doxygen2man'
+    props['DISTROCONFOPTS'] = ''
+    props['DISTCHECK_CONFIGURE_FLAGS'] = ''
+    props['MAKERPMOPTS'] = ''
 
     if (agentName.startsWith("debian-unstable-cross")) {
 	if (localinfo['ARCH'] == 'ARM') {
 	    props['compiler'] = 'arm-linux-gnueabihf-gcc'
-	    props['DISTROCONFOPTS'] = ' --host=arm-linux-gnueabihf --target=arm-linux-gnueabihf --disable-wireshark-dissector'
+	    props['DISTROCONFOPTS'] += ' --host=arm-linux-gnueabihf --target=arm-linux-gnueabihf --disable-wireshark-dissector'
 	}
     }
     if (agentName.startsWith("openindiana")) {
-	props['DISTROCONFOPTS'] = ' --disable-crypto-nss --disable-wireshark-dissector'
-	props['DISTCHECK_CONFIGURE_FLAGS'] = ' --disable-crypto-nss --disable-wireshark-dissector'
+	props['DISTROCONFOPTS'] += ' --disable-crypto-nss --disable-wireshark-dissector'
+	props['DISTCHECK_CONFIGURE_FLAGS'] += ' --disable-crypto-nss --disable-wireshark-dissector'
     }
+
+    // Check wireshark version - it needs >= 4.6.0
+    def String ws_ver_check = sh(returnStatus: true, script: '''
+                          pkg-config --atleast-version 4.6.0 wireshark
+                          echo $?
+                          ''')
+    if (ws_ver_check == '1') {
+	props['DISTROCONFOPTS'] += ' --disable-wireshark-dissector'
+	props['DISTCHECK_CONFIGURE_FLAGS'] += ' --disable-wireshark-dissector'
+	props['MAKERPMOPTS'] += ' --disable-wireshark-dissector'
+    }
+
 
     // Which job types to run debug options in
     props['DEBUGJOBS'] = ['voting', 'nonvoting', 'nonvoting-clang']

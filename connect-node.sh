e0204687af83717645fc92134684f6453cf6e962
Christine Caulfield <ccaulfie@redhat.com>
2026-03-05 13:41:26 +0000

    remove wget from start_node as the jar file is done in connect-node.sh
    make connect-node wget use localhost for efficiency

--- connect-node.sh
+++ connect-node.sh
@@ -2,5 +2,5 @@
 # Copy the Jenkins jar file to the remote node in $1
 # and run the start_node script
 
-curl https://ci.kronosnet.org/jnlpJars/agent.jar | ssh $1 "cat > /root/bin/agent.jar"
+curl http://localhost:8080/jnlpJars/agent.jar | ssh $1 "cat > /root/bin/agent.jar"
 ssh $1 /root/bin/start_node

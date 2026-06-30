39dc0d355f3628da8f94364758092ef24266b001
Ilias Romanos <iromanos@redhat.com>
2026-06-30 18:14:27 +0300

    updateOSNode: auto-detect ansible-managed nodes on mark_node_offline()

--- pipelines/libs/global/vars/updateOSNode.groovy
+++ pipelines/libs/global/vars/updateOSNode.groovy
@@ -10,7 +10,7 @@ def update_node(String agentName, Map info, String realNode)
 
     node("${agentName}") {
 	cleanWs(disableDeferredWipeout: true, deleteDirs: true)
-	mark_node_offline(realNode)
+	mark_node_offline(realNode, agentName)
 
 	try {
 	    runWithArtifacts(info, "update_${agentName}.log", {
@@ -73,7 +73,7 @@ def call(String agentName, Map info, String realNode)
     update_node(agentName, info, realNode)
 }
 
-def mark_node_offline(String nodeName)
+def mark_node_offline(String nodeName, String agentName)
 {
     for (aSlave in hudson.model.Hudson.instance.slaves) {
 	def computer = aSlave.getComputer();
@@ -84,12 +84,7 @@ def mark_node_offline(String nodeName)
 	    println("Waiting for node ${nodeName} to be offline")
 	    computer.waitUntilOffline()
 	    println("${nodeName} is now offline")
-	    // special case for ansible managed nodes
-	    // once the transition is done then workers should be set to 0 for all
-	    def workers = 1
-	    if (nodeName in ['freebsd-devel-x86-64', 'alpine-x86-64']) {
-		workers = 0
-	    }
+	    def workers = (nodeName == agentName) ? 1 : 0
 	    while (computer.countBusy() != workers) {
 		println("Waiting 300 seconds for node ${nodeName} to be idle")
 		sleep(300)

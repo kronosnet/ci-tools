078069d3d9b118b922a98553756f916328366351
Michal Pospíšil <mpospisi@redhat.com>
2026-08-25 15:31:45 +0200

    add configuration for pcs-0.12 pipeline
    This splits the old main pipeline into pcs-0.12 and main pipeline. It
    splits the stable pacemaker into the pcs-0.12 pipeline and pacemaker
    main into the pipeline for pcs main branch.
    
    When Pacemaker forks the branch for 3.1, and the corrsponding pipeline
    is added, a new pipeline for pcs-1.x will be used to test against it.

--- pipelines/projects/pcs/vars/getPublishBranches.groovy
+++ pipelines/projects/pcs/vars/getPublishBranches.groovy
@@ -1,5 +1,5 @@
 // Return list of branches, which will have their RPMs and Coverity scan results published
 def call()
 {
-    return ['main', 'pcs-0.10', 'pcs-0.11']
+    return ['main', 'pcs-0.10', 'pcs-0.11', 'pcs-0.12']
 }

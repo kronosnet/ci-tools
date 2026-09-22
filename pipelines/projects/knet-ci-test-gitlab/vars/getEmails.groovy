a1cf61647126aaa4ef8fd23a8616e73411d6727f
Ilias Romanos <iromanos@redhat.com>
2026-09-22 14:34:25 +0300

    DO NOT MERGE: only notify me when running jobs from this branch

--- pipelines/projects/knet-ci-test-gitlab/vars/getEmails.groovy
+++ pipelines/projects/knet-ci-test-gitlab/vars/getEmails.groovy
@@ -1,4 +1,4 @@
 // Users to email if a non-voting job fails as a comma-separated list
 def call() {
-    return 'fabbione@redhat.com, iromanos@redhat.com, arajko@redhat.com, averenin@redhat.com'
+    return 'iromanos@redhat.com'
 }
